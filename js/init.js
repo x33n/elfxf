946d82c70169ea3a549ac46097085343ce691ed6
Alex Gross <all@growww.pro>
2016-03-27 01:02:06 +0300

    projects bugfix, some fill

--- js/init.js
+++ js/init.js
@@ -11,54 +11,54 @@ $(function() {
         $bgobj.css({ backgroundPosition: coords });
      });
 
- $('.projects').slick({
-      dots: true,
-
-  infinite: false,
-  speed: 800,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-
-  //lazyLoad: 'ondemand',
-    // mobileFirst: true,
-    responsive: [
-    {
-      breakpoint: 1440,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-    ]
-  });
+ //$('.projects').slick({
+ //     dots: true,
+ //
+ // infinite: false,
+ // speed: 800,
+ // slidesToShow: 3,
+ // slidesToScroll: 1,
+ //
+ // //lazyLoad: 'ondemand',
+ //   // mobileFirst: true,
+ //   responsive: [
+ //   {
+ //     breakpoint: 1440,
+ //     settings: {
+ //       slidesToShow: 4,
+ //       slidesToScroll: 4,
+ //       infinite: true,
+ //       dots: true
+ //     }
+ //   },
+ //   {
+ //     breakpoint: 1024,
+ //     settings: {
+ //       slidesToShow: 3,
+ //       slidesToScroll: 3,
+ //       infinite: true,
+ //       dots: true
+ //     }
+ //   },
+ //   {
+ //     breakpoint: 600,
+ //     settings: {
+ //       slidesToShow: 2,
+ //       slidesToScroll: 2
+ //     }
+ //   },
+ //   {
+ //     breakpoint: 480,
+ //     settings: {
+ //       slidesToShow: 1,
+ //       slidesToScroll: 1
+ //     }
+ //   }
+ //   // You can unslick at a given breakpoint now by adding:
+ //   // settings: "unslick"
+ //   // instead of a settings object
+ //   ]
+ // });
 
 $(document).on('click', '#projectForm', function(){
 

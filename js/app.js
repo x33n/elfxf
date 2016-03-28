2a7169edca0a27a75abb761d87e3ef1b7d2c288a
Alex Gross <all@growww.pro>
2016-03-28 20:51:20 +0300

    projects to json

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ angular.module('cloverApp', []).
 	controller('ProjectsController', function($scope, $http){
 	$http.get('../projects/projectsdb.json').success(function(data){
 		$scope.projects = data;
+		console.log($scope.projects);
 	});
 });
 

cf941e7a7ab78ea4f6732acba7b6ba49bd05afd7
Alex Gross <all@growww.pro>
2016-04-06 20:35:17 +0300

    модалка описания проекта поправил блок персонального проекта для мобилы

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('cloverApp', []).
+angular.module('cloverApp', ['idialog', 'ngAnimate']).
 	directive('onLastRepeat', function() {
 		return function(scope, element, attrs) {
 			if (scope.$last) setTimeout(function(){
@@ -9,43 +9,6 @@ angular.module('cloverApp', []).
 	.controller('ProjectsController', function($scope, $http){
 		$scope.projects = [];
 		$scope.$on('onRepeatLast', function(scope, element, attrs){
-
-			//jQuery("#grid-container").cubeportfolio('destroy');
-			//jQuery("#grid-container").cubeportfolio({
-			//		defaultFilter: '*',
-			//		animationType: 'rotateSides',
-			//		gapHorizontal: 10,
-			//		gapVertical: 10,
-			//		gridAdjustment: 'responsive',
-			//		caption: 'overlayBottom',
-			//		displayType: 'fadeIn',
-			//		displayTypeSpeed: 100,
-			//		singlePageInlineDelegate: '.cbp-singlePageInline',
-			//		singlePageInlinePosition: 'below',
-			//		singlePageInlineShowCounter: true,
-			//		singlePageInlineInFocus: true,
-			//		singlePageInlineCallback: function (url, element) {
-            //
-			//			// to update singlePageInline content use the following method: this.updateSinglePageInline(yourContent)
-			//			var t = this;
-            //
-			//			$.ajax({
-			//					url: url,
-			//					type: 'GET',
-			//					dataType: 'html',
-			//					timeout: 5000
-			//				})
-			//				.done(function (result) {
-            //
-			//					t.updateSinglePageInline(result);
-            //
-			//				})
-			//				.fail(function () {
-			//					t.updateSinglePageInline("Error! Please refresh the page!");
-			//				});
-			//		}
-			//}
-			//);
 			console.log("projects loaded");
 			var swiper = new Swiper('.swiper-container', {
 				pagination: '.swiper-pagination',
@@ -70,10 +33,23 @@ angular.module('cloverApp', []).
 			});
 
 		});
-	$http.get('../projects/projectsdb.json')
-		.success(function(data){
-		$scope.projects = data;
-		console.log($scope.projects);
+
+		$scope.projectInfoBtn = function(selectedPrj){
+			$scope.selectedPrj = simpleKeys(selectedPrj);
+			console.log(selectedPrj);
+		};
+
+		$http.get('../projects/projectsdb.json')
+			.success(function(data){
+			$scope.projects = data;
+			console.log($scope.projects);
+
+
+
+
+
+
+
 	});
 });
 

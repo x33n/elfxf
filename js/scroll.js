1e386017cd3b873841e1441072e75566407ce578
Alex Gross <all@growww.pro>
2016-04-14 14:42:55 +0300

    добавил несколько описаний для старых пр поправил скролл для меню

--- js/scroll.js
+++ js/scroll.js
@@ -124,7 +124,7 @@ window.smoothScroll = (function (window, document, undefined) {
     var updateURL = overrides.updateURL || options.updateURL;
 
     // Selectors and variables
-    var headerHeight = 55;
+    var headerHeight = 0;
     var startLocation = window.pageYOffset; // Current location on the page
     var endLocation = _getEndLocation( document.querySelector(anchor), headerHeight ); // Scroll to location
     var animationInterval; // interval timer

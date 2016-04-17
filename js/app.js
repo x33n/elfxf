4234acbfc679f7e9cb733d436c1cc7912b1937df
Alex Gross <all@growww.pro>
2016-04-17 23:35:19 +0300

    форма звонка  заработала, карусель сбрасывается

--- js/app.js
+++ js/app.js
@@ -1,117 +1,140 @@
-angular.module('cloverApp', ['ngDialog']).
-	directive('onLastRepeat', function() {
-		return function(scope, element, attrs) {
-			if (scope.$last) setTimeout(function(){
-				scope.$emit('onRepeatLast', element, attrs);
-			}, 1);
-		};
-})
+angular.module('cloverApp', ['ngDialog'])
 
-	.config(function (ngDialogProvider) {
-		ngDialogProvider.setForceHtmlReload(true);
-	})
+    .directive('onLastRepeat', function () {
+        return function (scope, element, attrs) {
+            if (scope.$last) setTimeout(function () {
+                scope.$emit('onRepeatLast', element, attrs);
+            }, 1);
+        };
+    })
 
-	.controller('ProjectsController', function($scope, $http, $q, ngDialog){
-		$scope.projects = [];
-		$scope.prj = [];
-		$scope.$on('onRepeatLast', function(scope, element, attrs){
-			console.log("projects  carousel html structure loaded");
-			var swiper = new Swiper('.projects-carousel', {
-				pagination: '.swiper-pagination',
-				//setWrapperSize: true,
-				slidesPerView: 'auto',
-				centeredSlides: true,
-				paginationClickable: true,
-				paginationType: 'fraction',
-				nextButton: '.swiper-button-next',
-				prevButton: '.swiper-button-prev',
-				spaceBetween: 20,
-				speed: 400,
-				grabCursor: true,
-				initialSlide: 1,
+    .directive('onLastestRepeat', function () {
+        return function (scope, element, attrs) {
+            if (scope.$last) setTimeout(function () {
+                scope.$emit('onRepeatLastest', element, attrs);
+            }, 1);
+        };
+    })
 
-				//autoplay: 5000,
-				slideToClickedSlide: true,
-				lazyLoading: false,
-				lazyLoadingInPrevNextAmount: 5,
-				lazyLoadingOnTransitionStart: true
+    .controller('ProjectsController', function ($scope, $http, $q, ngDialog) {
+        $scope.projects = [];
+        $scope.prj = [];
+        $scope.$on('onRepeatLast', function (scope, element, attrs) {
+            console.log("projects  carousel html structure loaded");
+            var projectsCarousel = new Swiper('.projects-carousel', {
+                pagination: '.swiper-pagination',
+                //setWrapperSize: true,
+                slidesPerView: 'auto',
+                centeredSlides: true,
+                paginationClickable: true,
+                paginationType: 'fraction',
+                nextButton: '.swiper-button-next',
+                prevButton: '.swiper-button-prev',
+                spaceBetween: 20,
+                speed: 400,
+                grabCursor: true,
+                initialSlide: 1,
 
-			});
+                //autoplay: 5000,
+                slideToClickedSlide: true,
+                lazyLoading: false,
+                lazyLoadingInPrevNextAmount: 5,
+                lazyLoadingOnTransitionStart: true
 
-
-		});
-
-		$http.get('../projects/projectsdb-new.json')
-			.success(function(data){
-				$scope.projects = data;
-
-			});
-
-		$scope.showProjectInfo = function(project){
-			$scope.prj = $scope.projects[project];
-			console.log("получено значение: " + $scope.prj['name']);
-			ngDialog.open({
-				template: '../templates/pages/projects/projectinfo.html'
-			}).then(setTimeout(function(){
-				var galleryTop = new Swiper('.gallery-top', {
-					spaceBetween: 10,
-					preloadImages: false
-				});
-				var galleryThumbs = new Swiper('.gallery-thumbs', {
-					initialSlide: 0,
-					spaceBetween: 10,
-					centeredSlides: true,
-					slidesPerView: 'auto',
-					touchRatio: 0.2,
-					preloadImages: false,
-					slideToClickedSlide: true
-				});
-				galleryTop.params.control = galleryThumbs;
-				galleryThumbs.params.control = galleryTop;
-			}, 1000));
+            });
 
 
-			};
+        });
 
+        $http.get('../projects/projectsdb-new.json')
+            .success(function (data) {
+                $scope.projects = data;
 
+            });
 
-	})
-	.controller('CallbackFormController', function ($scope, $http, ngDialog) {
-		// initializing form
-		$scope.form = {
-			client: "",
-			phone: "",
-			time: ""
-		};
-		$scope.orderCall = function () {
-			ngDialog.open({
-				template: '/templates/forms/callback.html',
-				className: 'ngdialog-theme-flat',
-				scope: $scope
-			});
-			var inputTime = $('#inputTime').clockpicker({
-				placement: 'bottom',
-				align: 'left',
-				autoclose: true,
-				'default': 'now'
-			});
-		};
+        $scope.showProjectInfo = function (project) {
+            $scope.prj = $scope.projects[project];
+            console.log("получено значение: " + $scope.prj['name']);
+            ngDialog.open({
+                template: '../templates/pages/projects/projectinfo.html',
+                scope: $scope
+            });
+            $scope.$on('onRepeatLast', function (scope, element, attrs) {
 
-			// submit form
-		$scope.submitForm = function (form) {
-			// passing value to variable
-			var callForm = {
-				client: form.client,
-				phone: form.phone
-			};
-			if (callForm.client === "") {
-				callForm.client = "Не указано";
-			}
-		};
-
-	});
+                var galleryTop = new Swiper('.gallery-top', {
+                    spaceBetween: 10,
+                    preloadImages: false
+                });
+                var galleryThumbs = new Swiper('.gallery-thumbs', {
+                    initialSlide: 0,
+                    spaceBetween: 10,
+                    centeredSlides: true,
+                    slidesPerView: 'auto',
+                    touchRatio: 0.2,
+                    preloadImages: false,
+                    slideToClickedSlide: true
+                });
+                galleryTop.params.control = galleryThumbs;
+                galleryThumbs.params.control = galleryTop;
+                console.log("Gallery Initiated");
+                });
 
 
 
+        };
 
+
+    })
+    .controller('CallbackFormController', function ($scope, $http, ngDialog) {
+        // initializing form
+        $scope.form = {
+            client: "",
+            phone: "",
+            time: ""
+        };
+        $scope.orderCall = function () {
+            ngDialog.open({
+                template: '/templates/forms/callback.html',
+                className: 'ngdialog-theme-flat',
+                scope: $scope
+            });
+            var inputTime = $('#inputTime').clockpicker({
+                placement: 'bottom',
+                align: 'left',
+                autoclose: true,
+                'default': 'now'
+            });
+        };
+
+
+
+        $scope.submitForm = function (form) {
+            console.log("form submitted");
+            // passing value to variable
+            var callForm = {
+                client: form.client,
+                phone: form.phone
+            };
+            if (callForm.client === "") callForm.client = "Не указано";
+            console.log("Имя: " + callForm.client);
+            //$http({
+            //    method: 'POST',
+            //    url: "mail.php",
+            //    data: {
+            //        'email': $scope.form.mail,
+            //        'subject': $scope.form.subject,
+            //        'message': $scope.form.msg
+            //    },
+            //    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            //}).success(function (data, status, headers, config) {
+            //    $scope.status = status;
+            //    /* for debug mode*/
+            //    $scope.data = data;
+            //}).error(function (data, status, headers, config) {
+            //    $scope.status = status;
+            //    /* for debug mode*/
+            //    $scope.data = data || "Request failed";
+            //});
+        };
+    });
 
